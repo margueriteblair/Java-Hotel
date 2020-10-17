79076862d26db1255f9da8379346271833b7b942
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-16 22:34:49 -0400

    included getters in the client class

--- src/com/mblair/Client.java
+++ src/com/mblair/Client.java
@@ -8,11 +8,29 @@ public class Client {
     private String phoneNumber; //nb: we have to put in the phone numbers as a string
     private int roomNumber;
     private String roomType;
+    Room room;
 
     public Client(String name, String phoneNumber, int partySize) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.partySize = partySize;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getPartySize() {
+        return partySize;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
 
 }

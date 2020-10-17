f880e21e5d470b34262c1cccb5bfd816c67b1241
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-16 22:08:56 -0400

    pushing changes

--- src/com/mblair/Main.java
+++ src/com/mblair/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Hotel javaHotel = new Hotel("Java Hotel",
+                new StandardRoom(),
+                new StandardRoom(),
+                new StandardRoom(),
+                new SuiteRoom(),
+                new SuiteRoom()
+                );
+        //create command line interface code to go below
+        //that way, when we run the main program, just like with the mortgage
+        //calculator, we're taken through the steps of the program
     }
 }

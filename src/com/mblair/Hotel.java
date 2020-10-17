60fafc27ac3852a182415179b532b3b1559c1ffa
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-16 20:52:58 -0400

    setting up hashmaps for hotel class

--- src/com/mblair/Hotel.java
+++ src/com/mblair/Hotel.java
@@ -1,4 +1,12 @@
 package com.mblair;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Hotel {
+    private String hotelName;
+    private int numOfRooms;
+
+    Map<Integer,Room> allRooms = new HashMap<>();
+
 }

081fa8bcdb06f4f0c35c9a90617442c00527ecdc
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-17 17:46:20 -0400

    pushing changes

--- src/com/mblair/StandardRoom.java
+++ src/com/mblair/StandardRoom.java
@@ -1,15 +1,26 @@
 package com.mblair;
 
 public class StandardRoom extends Room {
+
+    private int rooms; //number of rooms
+    private int beds; //number of beds, distinguishes from a suite!
+
     @Override
     public boolean reserve(Client client) {
         return super.reserve(client);
     }
 
     public StandardRoom() {
-        super(averagePrice, roomNum, "single", roomFloor);
+        super(averagePrice, roomNum, roomType, getRoomFloor); //use getters to get these bad mamma jammas
         rooms = 1;
         beds = 1;
-        //constructor baby!
     }
+
+
+//    public StandardRoom() {
+//        super(averagePrice, roomNum, "single", roomFloor);
+//        rooms = 1;
+//        beds = 1;
+//        //constructor baby!
+//    }
 }

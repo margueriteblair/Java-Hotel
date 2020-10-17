99b901eeb4e6f85ac23bb363810e04a78319a61a
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-16 21:36:02 -0400

    laying foundation for hotel suite room class methods

--- src/com/mblair/SuiteRoom.java
+++ src/com/mblair/SuiteRoom.java
@@ -1,4 +1,20 @@
 package com.mblair;
 
 public class SuiteRoom extends Room {
+    private String roomType;
+    private boolean needsRestock;
+    //
+
+    public void overrideReserve() {
+        //include a check on needsRestock then run standard reserve
+    }
+
+    public void overrideCheckout() {
+        //set needsRestock to true run standard checkout
+    }
+
+    public void setRestock() {
+        needsRestock = false;
+    }
+
 }

ea141788207778e880c715d61ed3bb88bde2bcf1
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-18 16:42:59 -0400

    deleted irrelevant information

--- src/com/mblair/Client.java
+++ src/com/mblair/Client.java
@@ -3,8 +3,6 @@ package com.mblair;
 public class Client {
     private String name;
     private int partySize;
-    private int currentBill;
-    private double prepaidAmt;
     private String phoneNumber; //nb: we have to put in the phone numbers as a string
     private int roomNumber;
     private String roomType;

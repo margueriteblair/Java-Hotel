1a1f21faac065ef2b96dfd0087e632fabb468fa0
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-18 16:47:04 -0400

    more client methods

--- src/com/mblair/Client.java
+++ src/com/mblair/Client.java
@@ -3,6 +3,8 @@ package com.mblair;
 public class Client {
     private String name;
     private int partySize;
+    private int currentBill;
+    private int prepaidAmt;
     private String phoneNumber; //nb: we have to put in the phone numbers as a string
     private int roomNumber;
     private String roomType;
@@ -13,6 +15,23 @@ public class Client {
         this.phoneNumber = phoneNumber;
         this.partySize = partySize;
     }
+
+    public void makePayment(int newAmount) {
+        //increase the prepaid amount
+        prepaidAmt += newAmount;
+    }
+
+    public void chargeRoom(int newAmount) {
+        //this method increases the current bill
+        currentBill += newAmount;
+    }
+
+    public int getOutstanding() {
+        //returns the outstanding balance
+        return currentBill - prepaidAmt;
+
+    }
+
 
 
     //setter and getter functions
